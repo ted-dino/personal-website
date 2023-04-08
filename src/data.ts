642b5f4b254c7d99385e20002bdd2e591187a658
Ted Diño <ted23@tutanota.com>
2023-04-08 17:29:28 +0800

    Feat: add and style footer

--- src/data.ts
+++ src/data.ts
@@ -25,3 +25,21 @@ export const navLinks = [
     logo: "M16 2H8C4.691 2 2 4.691 2 8v13a1 1 0 0 0 1 1h13c3.309 0 6-2.691 6-6V8c0-3.309-2.691-6-6-6zm4 14c0 2.206-1.794 4-4 4H4V8c0-2.206 1.794-4 4-4h8c2.206 0 4 1.794 4 4v8z",
   },
 ];
+
+export const footerSocialLink = [
+  {
+    url: "mailto:7vhf5zek3@mozmail.com",
+    alt: "email",
+    src: "/footer-links/email.svg",
+  },
+  {
+    url: "https://www.upwork.com/freelancers/~01f8138f451bb334e9",
+    alt: "upwork",
+    src: "/footer-links/upwork.svg",
+  },
+  {
+    url: "https://www.linkedin.com/in/ted-dino/",
+    alt: "linkedin",
+    src: "/footer-links/linkedin.svg",
+  },
+];
