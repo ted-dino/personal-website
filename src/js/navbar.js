6a5ff947cf3e37caca1bd8c26d67be09d4a05150
Ted Diño <ted23@tutanota.com>
2023-04-10 00:02:10 +0800

    Fix: navbar bug

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,30 +1,49 @@
-const navLinks = document.querySelectorAll(".nav__link");
-const sections = document.querySelectorAll("section");
 const header = document.querySelector(".header");
+const sections = document.querySelectorAll("section[id]");
+const footer = document.querySelector("footer");
+const navLinks = document.querySelectorAll(".nav__list a[href]");
 
-const navLinksOptions = {
-  rootMargin: "-50px",
-  threshold: 0.5,
-};
+let currentSection = sections[0].getAttribute("id");
 
-const navLinksObserver = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      let sectionId = entry.target.getAttribute("id");
-      let navLink = document.querySelector(
-        `nav .nav__link[href="#${sectionId}"]`
-      );
-      navLinks.forEach((link) => {
-        link.removeAttribute("aria-current");
-      });
+navLinks[0].setAttribute("aria-current", "page");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((section) => {
+    const sectionHeight = section.offsetHeight;
+    const sectionTop = section.offsetTop - 150;
+    const sectionId = section.getAttribute("id");
+    const navLink = document.querySelector(
+      `.nav__list a[href="#${sectionId}"]`
+    );
+
+    if (footer.getBoundingClientRect().top <= 688) {
+      document
+        .querySelector(".nav__list a[href*=contact]")
+        .setAttribute("aria-current", "page");
+      currentSection = "contact";
+    } else {
+      document
+        .querySelector(".nav__list a[href*=contact]")
+        .removeAttribute("aria-current");
+      currentSection = sectionId;
+    }
+
+    if (
+      scrollY > sectionTop &&
+      scrollY <= sectionTop + sectionHeight &&
+      currentSection !== "contact"
+    ) {
+      navLinks.forEach((link) => link.removeAttribute("aria-current"));
       navLink.setAttribute("aria-current", "page");
+    } else {
+      navLink.removeAttribute("aria-current");
     }
   });
-}, navLinksOptions);
+}
 
-sections.forEach((section) => {
-  navLinksObserver.observe(section);
-});
+window.addEventListener("scroll", scrollActive);
 
 function scrollHeader() {
   if (this.scrollY >= 20) header.classList.add("scroll-header");
