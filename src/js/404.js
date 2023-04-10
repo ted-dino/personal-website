80fbbd9620439d052bcfa6e9152a57e2224d701c
Ted Diño <ted23@tutanota.com>
2023-04-10 18:02:20 +0800

    Feat: add 404 page

---
+++ src/js/404.js
@@ -0,0 +1,42 @@
+import confetti from "canvas-confetti";
+const easter = document.querySelector("#easter");
+const torch = document.querySelector(".torch");
+const modal = document.querySelector("#dialog");
+const closeBtn = document.querySelector("#class-modal");
+
+document.addEventListener("mousemove", function (event) {
+  torch.style.top = event.pageY + "px";
+  torch.style.left = event.pageX + "px";
+});
+
+easter.addEventListener("click", () => {
+  modal.show();
+  let interval = 0;
+
+  const timer = setInterval(() => {
+    interval++;
+    confetti({
+      particleCount: 100,
+      startVelocity: 50,
+      spread: 120,
+      origin: {
+        x: Math.random(),
+        y: Math.random() - 0.2,
+      },
+      ticks: 500,
+      disableForReducedMotion: true,
+      colors: ["#0f0e16", "#ffffff", "#a7a9be", "#ff8a05"],
+    });
+
+    if (interval === 10) {
+      clearInterval(timer);
+      confetti.reset();
+    }
+  }, 600);
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.close();
+  torch.remove();
+  easter.remove();
+});
